0f7c6ef4291e98b5a9948fbec030b5b11f990e75
Sunil H N <sunilh.n27@gmail.com>
2024-07-03 00:25:27 +0530

    added Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <section className="">
+        <hr className="my-4 border-gray-400"></hr>
+      <div className="flex justify-center items-center">Footer</div>
+    </section>
+  );
+}
+
+export default Footer;
