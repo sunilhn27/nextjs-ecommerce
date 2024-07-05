5a324eef6f7dd72cf138f9c4ff746eac4b4f9297
Sunil H N <sunilh.n27@gmail.com>
2024-07-06 00:23:38 +0530

    sidebar

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SideBar() {
+  return (
+    <section className="w-[15rem] border-2 border-gray-400 min-h-screen">
+      <div className="justify-center flex items-center">SideBar</div>
+    </section>
+  );
+}
+
+export default SideBar;
